0d671a69186d5e948898b9ed9a9fd7fbe4fe985a
BrenRdgz <Brenda.Rodriguez@theksquaregroup.com>
2021-11-04 21:19:00 -0600

    test

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const controllerTasks = require('../controllers/tasks.controller');
 const routerTasks = Router();
 
-routerTasks.get('/', controllerTasks.getTasks);
+routerTasks.get('/', checkUserAuth(), controllerTasks.getTasks);
 
 routerTasks.get('/:id', controllerTasks.getTaskById);
 
@@ -12,6 +12,10 @@ routerTasks.put('/:id', controllerTasks.updateTask);
 
 routerTasks.delete('/:id', controllerTasks.deleteTask);
 
+function checkUserAuth(req, res, next) {
+    if (req.session.user) return next();
+    return next(new NotAuthorizedError());
+  }
 
 
 
