1cf720dbc5f6b8ba8471d3f5a8561092ff39cb6e
BrenRdgz <Brenda.Rodriguez@theksquaregroup.com>
2021-11-04 23:06:34 -0600

    previousConnection

--- src/db.connection.js
+++ src/db.connection.js
@@ -1,35 +1,11 @@
 const mongoose = require('mongoose');
 
-const connectionDB = () => {
-        mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        mongoose.connection.on('error', function(){
-            console.log('It was occur an error');
-        });
-}
-
-/*database (){ 
-    mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    mongoose.connection.on('error', function(){
-        console.log('It was occur an error');
-    })
-}*/
-
-/*const connectionDB = async ()=>{
+const connectionDB = async () => {
     try{
-        await  mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        await mongoose.connect.on('error', function (){
-            console.log('it can not connect to DB');
-        })
-    }
+        const DB = await mongoose.connect('mongodb+srv://3HCMq1UPAL1whKfR:3HCMq1UPAL1whKfR@brencluster.irzmg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+    }catch (error){
+        console.log(error);
+    }  
 }
-*/
+
 module.exports = connectionDB;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const mongoose = requiere('mongoose');
 const connectionDB = require ('./db.connection');
 const routerTasks = require('./routes/tasks.routes');
 const app = express();
+
 connectionDB();
 
 app.set("PORT", process.env.PORT || 3000);
