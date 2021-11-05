017346d49d0ad00128d318023c393996ef9f67ad
BrenRdgz <Brenda.Rodriguez@theksquaregroup.com>
2021-11-04 21:37:00 -0600

    timeout2

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const connectionDB = require ('./db.connection');
 const routerTasks = require('./routes/tasks.routes');
 const app = express();
-const timeout = requiere ('connect-timeout');
 connectionDB();
 
 app.set("PORT", process.env.PORT || 3000);
-app.use(timeout('10s'));
+app.use(function(req, res, next) {
+    res.setTimeout(200);
+    next();
+  });
 app.use(express.json());
 app.get('/', (req, res) =>{
     res.send('Hi');
